fcb006bea6d83690b013baa72e6ed2a98334b091
player348494839 <92332270+player348494839@users.noreply.github.com>
2021-11-24 15:45:48 -0300

    Add files via upload

--- PROJECT-TEMPLATE_C11-main/sketch.js
+++ PROJECT-TEMPLATE_C11-main/sketch.js
@@ -1,24 +1,35 @@
 var ship,shipImg1;
 var sea,seaImg;
-ship.velocityX =-2;
-ship.velocityX =2;
+
 function preload(){
   seaImg = loadImage("sea.png");
-  shipImg1 = loadAnimation("ship-1.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
   
 }
 
 function setup(){
   createCanvas(400,400);
-  
+  sea = createSprite(200,200);
+  sea.addAnimation("sea.png");
+  sea.scale = 0.5;
+
+  ship = createSprite(200,200);
+  ship.addAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+  ship.scale = 0.3
 }
 
 function draw() {
   background("blue");
+  ship.velocityX =-2;
+  ship.velocityX =2;
+
     drawSprites();
+    sea.velocityX = 2;
     sea.addImage(seaImg);
-    if(sea.x < 0){
-      sea.x = sea.width/2;
+    if(sea.x >  400){
+      sea.x = 200;
+
+      drawSprites();
     }
  
 }
